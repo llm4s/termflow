52fb28f41ac5cedcecece76b875bf019ca65a040
Rory Graves <rory.graves@fieldmark.co.uk>
2026-06-15 05:13:04 +0100

    Layout prop tests (#213)
    Co-authored-by: Rory Graves <rory.graves@thetradedesk.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,10 @@ object Versions {
   val jline     = "3.30.6"
   val pureconfig = "0.17.10"
   val scalatest = "3.2.19"
+  // ScalaCheck and the matching scalatest+scalacheck bridge. The bridge
+  // version tracks the scalatest version (3.2.19.x) and targets scalacheck 1.18.
+  val scalacheck     = "1.18.1"
+  val scalatestPlusSc = "3.2.19.0"
 }
 
 object Deps {
@@ -14,4 +18,7 @@ object Deps {
   val pureconfigGenericScala3 =
     "com.github.pureconfig" %% "pureconfig-generic-scala3" % Versions.pureconfig
   val scalatest             = "org.scalatest" %% "scalatest" % Versions.scalatest
+  val scalacheck            = "org.scalacheck" %% "scalacheck" % Versions.scalacheck
+  val scalatestPlusScalacheck =
+    "org.scalatestplus" %% "scalacheck-1-18" % Versions.scalatestPlusSc
 }
